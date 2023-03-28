b4f5a2216ad4efc36dee771789d56947efb49e5a
lauritorma <bgu171@myy.haaga-helia.fi>
2023-03-28 16:09:41 +0300

    Implemented navigation between gamesessions-,createsession- and profile-screens

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Tab = createBottomTabNavigator();
+
+function GameSessionsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.font}>Game sessions</Text>
+    </View>
+  );
+}
+
+function CreateSessionScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.font}>Here you create sessions</Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.font}>Here is your profile</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+      
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'Games') {
+              iconName = 'gamepad';
+            } else if (route.name === 'Create') {
+              iconName = 'plus-square';
+            } else if (route.name === 'Profile') {
+              iconName = 'house-user';
+            }
+
+            return <FontAwesome5 name={iconName} size={size} color={color} />;
+          },
+
+          tabBarActiveTintColor: '#0088B4',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: { ...styles.tabBarStyle },
+          headerStyle: {
+            backgroundColor: '#0088B4',
+            height: '6%',
+          },
+          headerTitleStyle: {
+            color: '#0088B4',
+          }
+          
+        })}
+      >
+        <Tab.Screen name="Games" component={GameSessionsScreen} />
+        <Tab.Screen name="Create" component={CreateSessionScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    
+  },
+
+  font: {
+    color: 'white'
+  },
+
+  tabBarStyle: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    paddingBottom: 10,
+    paddingTop: 10,
+    backgroundColor: 'black',
+    borderTopWidth: 1,
+    borderTopColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
+    height: 60,
+    zIndex: 999,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
   },
 });
